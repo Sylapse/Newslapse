383d8f210fb9fa6c21b9643df1beebfecc425250
Tom Pratt <tinglybill@outlook.com>
2015-07-08 14:55:32 +0100

    Added MvvmCross to Core and Droid

--- News.Droid/MainActivity.cs
+++ News.Droid/MainActivity.cs
@@ -9,27 +9,27 @@ using Android.OS;
 
 namespace News.Droid
 {
-    [Activity (Label = "News.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
-    {
-        int count = 1;
-
-        protected override void OnCreate (Bundle bundle)
-        {
-            base.OnCreate (bundle);
-
-            // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-            button.Click += delegate {
-                button.Text = string.Format ("{0} clicks!", count++);
-            };
-        }
-    }
+//    [Activity (Label = "News.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+//    public class MainActivity : Activity
+//    {
+//        int count = 1;
+//
+//        protected override void OnCreate (Bundle bundle)
+//        {
+//            base.OnCreate (bundle);
+//
+//            // Set our view from the "main" layout resource
+//            SetContentView (Resource.Layout.Main);
+//
+//            // Get our button from the layout resource,
+//            // and attach an event to it
+//            Button button = FindViewById<Button> (Resource.Id.myButton);
+//			
+//            button.Click += delegate {
+//                button.Text = string.Format ("{0} clicks!", count++);
+//            };
+//        }
+//    }
 }
 
 

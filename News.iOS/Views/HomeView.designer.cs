37475585374088592e3cbc9ace45c677b018bb6f
Tom <tinglybill@outlook.com>
2015-07-09 13:04:23 +0100

    Added and bound HomeView

---
+++ News.iOS/Views/HomeView.designer.cs
@@ -0,0 +1,34 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace News.iOS
+{
+	[Register ("HomeView")]
+	partial class HomeView
+	{
+		[Outlet]
+		UIKit.UIActivityIndicatorView ActivityIndicator { get; set; }
+
+		[Outlet]
+		UIKit.UITableView TableView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (ActivityIndicator != null) {
+				ActivityIndicator.Dispose ();
+				ActivityIndicator = null;
+			}
+
+			if (TableView != null) {
+				TableView.Dispose ();
+				TableView = null;
+			}
+		}
+	}
+}
